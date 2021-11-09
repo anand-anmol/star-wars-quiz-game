799aa54d9f0bfa9cf7583efea78c9e813a949eef
Anmol <animol150>
2021-11-09 08:26:35 -0800

    added docker-compose

--- storage/create_tables_mysql.py
+++ storage/create_tables_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="localhost", user="root", password="password", database="leaderboard")
+db_conn = mysql.connector.connect(host="10.5.0.5", user="root", password="password", database="leaderboard")
 
 db_cursor = db_conn.cursor()
 
