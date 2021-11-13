6ec956d22256c5760ff45d5963d61c284528dd4e
Anmol <animol150>
2021-11-13 13:18:24 -0800

    changed database user from root

--- storage/drop_tables_mysql.py
+++ storage/drop_tables_mysql.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-db_conn = mysql.connector.connect(host="10.5.0.5", user="root", password="password", database="leaderboard")
+db_conn = mysql.connector.connect(host="10.5.0.5", user="user", password="password", database="leaderboard")
 
 db_cursor = db_conn.cursor()
 
